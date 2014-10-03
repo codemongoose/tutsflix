7072156193d8b550388b3c4142342c4e8c453247
CodeMongoose <ksdhanjal@gmail.com>
2014-10-03 05:50:11 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tutsflix::Application.config.secret_key_base = '5d1b786fba2b2a978c7cc9e4647e200759c52c79f5445abe08f759117ff96c9cfdad8e0bf20ee925ab9bf3e999851f51c64ff3ddb91edf8c785c42ee6a5235c8'
