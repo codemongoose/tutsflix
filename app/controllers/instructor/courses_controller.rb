4e5fc5f96a0705d17805f95fcb8b70d16f51403b
CodeMongoose <ksdhanjal@gmail.com>
2014-10-07 07:11:19 +0000

    Enable permission on instructor pages and refactor - ch 19

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,7 @@
 class Instructor::CoursesController < ApplicationController
 	before_action :authenticate_user!
+	before_action :require_authorized_for_current_course, :only => [:show]
+
 	def new
 		@course = Course.new
 	end
@@ -15,7 +17,7 @@ class Instructor::CoursesController < ApplicationController
 	end
 
 	def show
-		@course = Course.find(params[:id])
+		# @course = Course.find(params[:id])
 	end
 
 
@@ -23,6 +25,17 @@ class Instructor::CoursesController < ApplicationController
 
 
 	private
+
+	def require_authorized_for_current_course
+		if current_course.user != current_user
+			render :text => "Unauthorized", :status => :unauthorized
+		end
+	end
+
+	helper_method :current_course
+	def current_course
+		@current_course ||= Course.find(params[:id])
+	end
 
 	def course_params
 		params.require(:course).permit(:title, :description, :cost)
