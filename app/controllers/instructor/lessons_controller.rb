1c1bae0c60864beb3524f3c6e0442422be700b29
CodeMongoose <ksdhanjal@gmail.com>
2014-10-07 06:40:21 +0000

    Enable permission on Lessons and code refactoring - ch 18

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,19 +1,37 @@
 class Instructor::LessonsController < ApplicationController
-	def new
-		@section = Section.find(params[:section_id])
-		@lesson = Lesson.new
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_section
+
+
+def new # note direct use of helper method calls vs. variable for current section
+    # if current_section.course.user != current_user
+    #   return render :text => 'Unauthorized', :status => :unauthorized
+    # end
+    @lesson = Lesson.new
+end
+
+def create
+    # if current_section.course.user != current_user
+    #   return render :text => 'Unauthorized', :status => :unauthorized
+    # end
+    @lesson = current_section.lessons.create(lesson_params)
+    redirect_to instructor_course_path(current_section.course)
+end
+
+private
+
+def require_authorized_for_current_section
+	if current_section.course.user != current_user
+		return render :text => 'Unauthorized', :status => :unauthorized
 	end
+end
 
-	def create
-		@section = Section.find(params[:section_id])
-		@lesson = @section.lessons.create(lesson_params)
-		redirect_to instructor_course_path(@section.course)
-	end
+  helper_method :current_section  #allows view to call this and directly use current_section
+  def current_section
+  	@current_section ||= Section.find(params[:section_id])
+  end
 
-	private
-
-	def lesson_params
-		params.require(:lesson).permit(:title, :subtitle)
-	end
-
+  def lesson_params
+  	params.require(:lesson).permit(:title, :subtitle)
+  end
 end
